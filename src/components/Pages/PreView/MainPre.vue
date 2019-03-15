c994e8ff596b074e477797167fc73e4cd27d3825
luoyu <1093930651@qq.com>
2019-03-15 12:52:47 +0800

    fix

--- src/components/Pages/PreView/MainPre.vue
+++ src/components/Pages/PreView/MainPre.vue
@@ -10,11 +10,6 @@ export default {
   name: 'MainPre',
   methods:{
     init(data) {
-      var place = [];
-      for (var i = 0; i < data.length; i++) {
-        place.push(data[i].place)
-      }
-      place = Array.from(new Set(place))
       
       this.svg = d3.select("#MainPre_svg");
       this.width = document.getElementById("MainPre").offsetWidth - 44;
@@ -23,14 +18,18 @@ export default {
       this.padding = 20;
       this.interval = 1000;
       this.y = d3
-        .scaleTime()
+        .scaleLinear() 
         .range([this.height - this.padding - this.padding, 0]);
 
-      this.x = d3.scaleBand().range([0, (this.width - this.padding - this.padding)]);
+      this.x = d3.scaleBand().range([0, (this.width - this.padding - this.padding)])
 
-      this.y.domain([new Date(data[0].s), new Date(data[data.length - 1].e)]);
-      this.x.domain(place);
+      this.y.domain([0,d3.max(data.allcountrys,function(d){
+          return d.works.length;
+      })]);
+      this.x.domain(data.allcountrys.map(v=>v.name));
 
+      this.axisstep = this.x.step()
+      this.rectWidth =  (this.width - this.axisstep * 2) / 20 -10;
       this.yAxis = d3.axisRight(this.y);
       this.xAxis = d3.axisBottom(this.x);
     },
@@ -55,13 +54,37 @@ export default {
         .attr("fill", "black")
         .call(xAxis);
     },
-    drawLine(data) {
+    drawRect(data) {
       const svg = this.svg;
       const height = this.height;
       const padding = this.padding;
       const x = this.x;
-      const y1 = this.y1;
-    },
+      const y = this.y;
+      const axisstep = this.axisstep
+      const rectWidth = this.rectWidth
+
+      for (let i=0;i<data.allcountrys.length;i++){
+        svg.append('g')
+        .selectAll('rect')
+        .data(data.allcountrys[i].color)
+        .enter()
+        .append('rect')
+        .attr('class','bgc')
+        .attr('fill', function(d){
+            return 'rgb(' + d[0] + ',' + d[1] + ',' + d[2] + ')';
+        })
+        .attr('width',rectWidth)
+        .attr('height', function(d){
+          return height - padding*2;
+        })
+        .attr('x',function(d){
+          return x(data.allcountrys[i].name) + axisstep/2 - padding;
+        })
+        .attr('y', function(d,i){
+          return y(data.allcountrys[i].works.length) + padding
+        })
+      }
+    }
   },
   mounted(){
     var data = this.$store.getters.getOridata;
@@ -71,9 +94,9 @@ export default {
     var timer = setInterval(() => {
       if (document.getElementById("MainPre").offsetWidth != 0) {
         clearInterval(timer);
-        that.init(data);
-        that.drawAxis(data);
-        that.drawLine(data);
+        that.init(data.all);
+        that.drawAxis(data.all);
+        that.drawRect(data.all);
       }
     }, 10);
   }
